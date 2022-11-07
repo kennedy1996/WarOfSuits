527c7c323ac377ef7316ffbb0accfed84e974207
kennedy1996 <87372917+kennedy1996@users.noreply.github.com>
2022-11-07 17:30:11 +0000

    Implemented Initial Activity
    - Show Game's name
    - Option to Start a Game, and after start suffle cards and go to start game on another activity

--- app/src/main/java/com/wairofsuits/ui/MainActivity.kt
+++ app/src/main/java/com/wairofsuits/ui/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.wairofsuits.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 import com.wairofsuits.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonStart = findViewById<Button>(R.id.activity_main_button_start_game)
+        val imageStartGame = findViewById<ImageView>(R.id.activity_main_image_start_game)
+        buttonStart.setOnClickListener {
+            showAnimationStartGame(buttonStart, imageStartGame)
+            waitTimeToStartGame(buttonStart, imageStartGame)
+        }
+    }
+
+    private fun showAnimationStartGame(buttonStart: Button, imageStartGame: ImageView) {
+        buttonStart.visibility= View.INVISIBLE
+        imageStartGame.visibility=View.VISIBLE
+        Glide.with(this).load(R.drawable.riffle).into(imageStartGame)
+    }
+
+    private fun waitTimeToStartGame(buttonStart: Button, imageStartGame: ImageView) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, GameActivity::class.java)
+            startActivity(intent)
+            originalState(buttonStart, imageStartGame)
+        }, 4000)
+    }
+
+    private fun originalState(buttonStart: Button, imageStartGame: ImageView) {
+        buttonStart.visibility= View.VISIBLE
+        imageStartGame.visibility=View.INVISIBLE
     }
 }
